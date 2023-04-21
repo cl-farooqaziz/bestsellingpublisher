484c0a9ee5b022ffa778d1cd6fce7d8c84a648ff
alim-2023 <alim@crystallitedigital.com>
2023-04-21 22:25:27 +0500

    spac

--- pages/contact.js
+++ pages/contact.js
@@ -60,7 +60,7 @@ export default function Contact() {
       <Clients />
 
       {/*========= Bookshelf fold =========*/}
-      <Container fluid>
+      <Container fluid className='px-md-5'>
         <Row className='contact'>
           <Col md={6}>
             <Formdata />
