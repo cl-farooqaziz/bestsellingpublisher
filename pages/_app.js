eef31c87e5e1b3536953800e4f772cc8d177a64c
alim-2023 <alim@crystallitedigital.com>
2023-05-04 00:12:13 +0500

    remove chat

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,34 @@
 import '@/styles/globals.css'
 import Header from '../components/Header'
 import Footer from '@/components/Footer'
-const ZENDESK_KEY = "a97320cd-3363-42aa-a55b-7613ba478cd5";
-import Zendesk, { ZendeskAPI } from "../pages/ZendexConfig";
-import Link from 'next/link';
+// const ZENDESK_KEY = "a97320cd-3363-42aa-a55b-7613ba478cd5";
+// import Zendesk, { ZendeskAPI } from "../pages/ZendexConfig";
+// import Link from 'next/link';
 
 
 export default function App({ Component, pageProps }) {
 
-  const handleLoaded = () => {
-    // ZendeskAPI("messenger", "open");
-    zE('webWidget:on', 'open');
-  };
+  // const handleLoaded = () => {
+  //   // ZendeskAPI("messenger", "open");
+  //   zE('webWidget:on', 'open');
+  // };
 
-  function openZendeskChat(e) {
-    e.preventDefault();
-    if (typeof zE !== 'undefined') {
-      zE('webWidget', 'open')
-    }
-  }
+  // function openZendeskChat(e) {
+  //   e.preventDefault();
+  //   if (typeof zE !== 'undefined') {
+  //     zE('webWidget', 'open')
+  //   }
+  // }
 
   return (
     <>
       <Header />
       < Component {...pageProps} />
       <Footer />
-      <div>
+      {/* <div>
         <Zendesk defer zendeskKey={ZENDESK_KEY} onLoaded={handleLoaded} /> 
         <Link href="#" onClick={openZendeskChat}>Open Zendesk Chat</Link>
-      </div>
+      </div> */}
     </>
   )
 }
