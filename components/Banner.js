93789b4e1e8444eb53a4357acd169566b86756e6
alim-2023 <alim@crystallitedigital.com>
2023-05-05 20:20:02 +0500

    remove width + height

--- components/Banner.js
+++ components/Banner.js
@@ -147,9 +147,6 @@ const Banner = (props) => {
                                     <Image loading="lazy" className='img-fluid'
                                         src={props.imageban}
                                         alt='Best_Publisher'
-                                        width={621}
-                                        height={401}
-
                                     />
                                 </div>
                             </Fade>
